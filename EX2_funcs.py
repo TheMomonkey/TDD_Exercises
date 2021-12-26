ed02638d67f9a53b22abddbe23784600ed80c7ac
andrepnunes <a30nunes@gmail.com>
2021-12-26 05:21:42 +0100

    [FEAT] add 'add' feature - tests passed

--- EX2_funcs.py
+++ EX2_funcs.py
@@ -1,6 +1,12 @@
+import sqlite3
+
+conn = sqlite3.connect('db_test.db')
+conn.row_factory = sqlite3.Row
+c = conn.cursor()
 
 def add(username, password, spub, spriv, epub, epriv):
-	return
+	c.execute("INSERT INTO Utilisateurs (username, password, spublickey, sprivatekey, epublickey, eprivatekey) VALUES('" + username + "', '" + password + "', '" + spub + "', '" + spriv + "', '" + epub + "', '" + epriv + "')")
+	conn.commit()
 
 def login(username, password, spub, spriv, epub, epriv):
 	return
